a9945b5af86345a2da9504a85ee09edb5b6dcd88
Shavalino Fayola <duskfay@gmail.com>
2024-10-24 08:30:34 +0700

    perubahan index vendor dan footer dan link

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include "head.php";
         <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1">
           <h1><?= $home["headline"] ?></h1>
           <h2><?= $home["desc"] ?></h2>
-          <a href="contact.php" class="btn-get-started scrollto">Get Started</a>
+          <a href="form.php" class="btn-get-started scrollto">Get Started</a>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img">
           <img src="<?= $home["img"] ?>" class="img-fluid animated" alt="">
@@ -44,19 +44,8 @@ include "head.php";
 
   <?php
   include "footer.php"
+  include "vendor.php"
   ?>
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
 
 </body>
 
